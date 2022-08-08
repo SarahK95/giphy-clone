f772d4db20bf0bf646431e615b65a56c79d7966b
SarahKamunya <sarahkamunya254@gmail.com>
2022-08-08 18:46:03 +0300

    generate new gifts component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
+import { GifsComponent } from './gifs/gifs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchComponent
+    SearchComponent,
+    GifsComponent
   ],
 
   imports: [
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,8 @@ import { HttpClient } from '@angular/common/http';
 export class DataService {
 
   constructor(private http: HttpClient) { }
+
+  getTrendingGifs() {
+    return this.http.get('https://api.giphy.com/v1/gifs/trending? api_key=%$[environment.giphyApiKey]&limit=50');
+  }
 }
