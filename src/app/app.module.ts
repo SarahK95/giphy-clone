15f82a30ef298e5ca860793dfe5afbeb331a8855
SarahKamunya <sarahkamunya254@gmail.com>
2022-08-08 18:17:14 +0300

    import HttpClient in data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,10 +13,13 @@ import { SearchComponent } from './search/search.component';
     HeaderComponent,
     SearchComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
